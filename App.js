143f139d1f350af5cdff6ffac2f34ca607531ec2
Monasser112 <nasrmahmoud425@gmail.com>
2020-11-25 17:50:39 +0200

    Added SearchBar Component int HomeScreen

--- App.js
+++ App.js
@@ -1,22 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import HomeScreen from "./Screens/HomeScreen";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Testing the App</Text>
-      <Text>This is another Test</Text>
-      <StatusBar style="auto" />
+    <View>
+      <HomeScreen />
     </View>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+};
+export default App;
---
+++ Components/SearchBar.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { View, StyleSheet, TextInput } from "react-native";
+import { Feather } from "@expo/vector-icons";
+
+const SearchBar = () => {
+  const [searchterm, setSearchTerm] = useState("");
+
+  return (
+    <View style={styles.backgroundStyle}>
+      <Feather style={{ top: 10 }} name="search" size={30} />
+      <TextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        style={styles.inputStyle}
+        placeholder="Search"
+        value={searchterm}
+        onChangeText={setSearchTerm}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  backgroundStyle: {
+    marginTop: 30,
+    backgroundColor: "#F0EEEE",
+    height: 50,
+    marginHorizontal: 15,
+    borderRadius: 15,
+    flexDirection: "row",
+    marginBottom: 10,
+  },
+  inputStyle: {
+    flex: 1,
+    fontSize: 18,
+    marginLeft: 4,
+  },
+  iconStyle: {
+    fontSize: 35,
+    alignSelf: "center",
+    marginHorizontal: 15,
+  },
+});
+export default SearchBar;
